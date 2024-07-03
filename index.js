60e727f0ae93c04846ab0a0c9f179a0238f70c1b
Vikas Motla <vikas.motla@gmail.com>
2024-07-03 16:05:45 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+    res.status(200).json({Name:'vikas'});
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
