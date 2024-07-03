f9ee649ba0903c882c6a35b4ab2b969019a4aadf
Vikas Motla <vikas.motla@gmail.com>
2024-07-03 16:15:56 +0530

    minor

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    
+
     res.status(200).json({Name:'vikas'});
 });
 
